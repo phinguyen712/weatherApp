7354108a04d88929d0a0aeb16b9a89c0ec333bff
phi nguyen <phinguyen712@gmail.com>
2016-10-04 18:54:22 +0000

    beautify nav bar and add grid center to main component

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link} = require("react-router");
 
 
 var Examples = (props) => {
    return (
-      <h3>Examples Component</h3>
+      <div>
+         <h1 className="text-Center">Examples</h1>
+         <p>Here are a few queries to try out</p>
+         <ol>
+            <li>
+               <Link to="/?location=Florida">Florida</Link>
+            </li>
+            <li>
+               <Link to="/?Location=Taipei">TaiPei</Link>
+            </li>
+         </ol>
+      </div>
     );
 };
 
